e1ca2895a6f34c780b26f38e08a1ef2f7a020ab6
DrGooseman <jamesjquinn12@gmail.com>
2020-05-25 22:36:24 +0300

    removes some unneeded stuff from MVC&FS

--- MVC&FS/controllers/products.js
+++ MVC&FS/controllers/products.js
@@ -4,9 +4,6 @@ exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
   });
 };
 
@@ -22,9 +19,6 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: "Shop",
       path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
     });
   });
 };
