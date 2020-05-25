12767a9d162d0a0adb36a106fb5a7aa7dd942efe
DrGooseman <jamesjquinn12@gmail.com>
2020-05-25 12:49:51 +0300

    finishes pug setup

--- Pug/routes/admin.js
+++ Pug/routes/admin.js
@@ -1,15 +1,14 @@
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
@@ -17,4 +16,5 @@ router.post("/add-product", (req, res, next) => {
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
