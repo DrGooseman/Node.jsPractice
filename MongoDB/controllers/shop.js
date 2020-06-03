d6ae6e40043714ede9f6baeb9e6aa57a9cfea9de
DrGooseman <jamesjquinn12@gmail.com>
2020-06-03 23:47:33 +0300

    adds order model, can create an order

--- MongoDB/controllers/shop.js
+++ MongoDB/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -75,28 +76,24 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId.doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user._id,
+        },
+        products,
+      });
+      return order.save();
     })
     .then((result) => {
-      return fetchedCart.setProducts(null);
+      return req.user.clearCart();
     })
     .then((result) => {
       res.redirect("/orders");
