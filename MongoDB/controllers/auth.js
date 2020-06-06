f3f14639bdd8041b5eef031b32f58aeb0f5dbdb7
DrGooseman <jamesjquinn12@gmail.com>
2020-06-06 21:20:51 +0300

    adds user login functionality

--- MongoDB/controllers/auth.js
+++ MongoDB/controllers/auth.js
@@ -11,13 +11,21 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5ed79de1d1015146f4ab86e5")
+  const { email, password } = req.body;
+
+  User.findOne({ email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
+      if (!user) return res.redirect("/login");
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
+        }
+        res.redirect("/login");
       });
     })
     .catch((err) => console.log(err));
