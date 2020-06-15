92feed2b72c06400583db4a3c70a1c68eb2bcf2e
DrGooseman <jamesjquinn12@gmail.com>
2020-06-15 23:29:23 +0300

    adds a check for invoices to see if the user matches

--- MongoDB/controllers/shop.js
+++ MongoDB/controllers/shop.js
@@ -150,6 +150,13 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) return next(new Error("No order found."));
+      if (order.user.userId.toString() !== req.user._id.toString())
+        return next(new Error("Unauthorized."));
+    })
+    .catch((err) => next(err));
   const invoiceName = "invoice-" + orderId;
   const invoicePath = path.join("data", "invoices", invoiceName);
   fs.readFile(invoicePath, (err, data) => {
