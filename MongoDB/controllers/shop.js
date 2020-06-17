8df6d58f1ac262c694646f0a04668c583908a4b1
DrGooseman <jamesjquinn12@gmail.com>
2020-06-17 23:51:27 +0300

    creates invoice PDF on the fly and saves it

--- MongoDB/controllers/shop.js
+++ MongoDB/controllers/shop.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
+const PDFDocument = require("pdfkit");
+
 const Product = require("../models/product");
 const Order = require("../models/order");
+const order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -159,6 +162,37 @@ exports.getInvoice = (req, res, next) => {
     .catch((err) => next(err));
   const invoiceName = "invoice-" + orderId;
   const invoicePath = path.join("data", "invoices", invoiceName);
+
+  const pdfDoc = new PDFDocument();
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader(
+    "content-Disposition",
+    "inline; filename='" + invoiceName + "'"
+  );
+  pdfDoc.pipe(fs.createWriteStream(invoicePath));
+  pdfDoc.pipe(res);
+
+  pdfDoc.fontSize(26).text("Invoice", {
+    underline: true,
+  });
+  pdfDoc.text("-------------------");
+  let totalPrice = 0;
+  order.products.forEach((prod) => {
+    totalPrice += prod.quantity * prod.product.price;
+    pdfDoc
+      .fontSize(14)
+      .text(
+        prod.product.title +
+          " - " +
+          prod.quantity +
+          " x " +
+          "$" +
+          prod.product.price
+      );
+  });
+  pdfDoc.text("-------------------");
+  pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+  pdfDoc.end();
   // fs.readFile(invoicePath, (err, data) => {
   //   if (err) return next(err);
   //   res.setHeader("Content-Type", "application/pdf");
@@ -168,11 +202,8 @@ exports.getInvoice = (req, res, next) => {
   //   );
   //   res.send(data);
   // });
-  const file = fs.createReadStream(invoicePath);
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader(
-    "content-Disposition",
-    "inline; filename='" + invoiceName + "'"
-  );
-  file.pipe(res);
+
+  //const file = fs.createReadStream(invoicePath);
+
+  //file.pipe(res);
 };
