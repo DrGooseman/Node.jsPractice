75d893c03cb85bcbe8b8a704ba1be953cce94314
DrGooseman <jamesjquinn12@gmail.com>
2020-06-25 22:35:21 +0300

    adds validation for post creation

--- REST/controllers/feed.js
+++ REST/controllers/feed.js
@@ -1,14 +1,36 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/demo.jpg",
+        creator: {
+          name: "James",
+          createdAt: new Date(),
+        },
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(422).json({ message: "Validation fails.", errors });
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
     message: "Post Created.",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "James" },
+      createdAt: new Date().toISOString(),
+    },
   });
 };
