b0737539c7765e50dcb02a0fcfa11a41fbbd8fdc
DrGooseman <jamesjquinn12@gmail.com>
2020-06-27 17:08:08 +0300

    saves new posts in the database

--- REST/controllers/feed.js
+++ REST/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -17,20 +19,20 @@ exports.getPosts = (req, res, next) => {
   });
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty())
     return res.status(422).json({ message: "Validation fails.", errors });
   const title = req.body.title;
   const content = req.body.content;
+  const post = new Post({
+    title,
+    content,
+    creator: { name: "James" },
+  });
+  await post.save();
   res.status(201).json({
     message: "Post Created.",
-    post: {
-      id: new Date().toISOString(),
-      title,
-      content,
-      creator: { name: "James" },
-      createdAt: new Date().toISOString(),
-    },
+    post,
   });
 };
