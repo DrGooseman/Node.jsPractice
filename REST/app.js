c8cb17ad036c4abacac49d72b9eaa4011789dd5e
DrGooseman <jamesjquinn12@gmail.com>
2020-06-30 23:38:15 +0300

    adds user model and auth route/controller

--- REST/app.js
+++ REST/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
